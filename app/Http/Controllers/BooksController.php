da1c765f95757e5c1dee290983d7df175cb0524e
Karthick <karthiram165@gmail.com>
2020-03-12 15:44:54 +0530

    routes and books table fields was created

--- app/Http/Controllers/BooksController.php
+++ app/Http/Controllers/BooksController.php
@@ -3,20 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Book;
 
 class BooksController extends Controller
 {
     public function index()
     {
-        return 'Welcome';
-    }
-
-    public function greet($name)
-    {
-        return "My name is" . " " . $name;
-    }
-    public function view($name)
-    {
-        return view('test')->with('name', $name);
+        $books = Book::all(); // select * from books
+        return response()->json($books);
     }
 }
