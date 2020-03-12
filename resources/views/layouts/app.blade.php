9e7cd8bcca1c6818cca94ba1a9640af9cab4bf60
Karthick <karthiram165@gmail.com>
2020-03-12 16:02:07 +0530

    view layout was created

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,13 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="{{ asset('style.css') }}">
+    <title>BlankBook</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 </head>
 
 <body>
 
     @yield('content')
+
 </body>
 
 </html>
