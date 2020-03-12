95a484d1bb808ba0fa12a899242af9fe84ef5afd
Karthick <karthiram165@gmail.com>
2020-03-12 16:23:56 +0530

    navbar was created

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
 </head>
 
 <body>
-
+    @include('partials.navbar')
     @yield('content')
 
 </body>
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,10 @@
+<nav class="navbar navbar-dark bg-primary">
+    <div class="container">
+        <a href="/books" class="navbar-brand">BlankBook</a>
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-link">
+                <a href="#" class="nav-link">Created by John Doe</a>
+            </li>
+        </ul>
+    </div>
+</nav>
